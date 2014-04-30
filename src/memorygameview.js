24ee5daa0a3c9b6d629a82e4e718571debaaa6ed
jimenglish81 <jimenglish81@gmail.com>
2014-04-30 21:44:43 +0100

    modify view to render correctly

--- src/memorygameview.js
+++ src/memorygameview.js
@@ -1,17 +1,23 @@
-function MemoryGameView(game) {
+function MemoryGameView(game, cssSelector) {
    this._game = game;
+   this._jContainer = $(cssSelector);
 }
 
 MemoryGameView.prototype.render = function() {
    var jDom = MemoryGameView.TEMPLATE.clone();
-   jDom.append(this._game.grid.map(MemoryGameView.buildItem));
+   jDom.append(this._game.grid.map(MemoryGameView.prototype._buildItem, this))
+		.appendTo(this._jContainer);
+		
+   return this;
 };
 
-MemoryGame.prototype._buildItem = function(item) {
-   var jItem = MemoryGameView.ITEM_TEMPLATE.clone().text(item.value);
-   return MemoryGameView.ITEM_TEMPLATE.clone
+MemoryGameView.prototype._buildItem = function(item, index) {
+   var jItem = MemoryGameView.ITEM_TEMPLATE.clone()
+							.text(item.value)
+							.toggleClass('break', !(index % this._game.width));
+   return jItem;
 };
 
-MemoryGameView.TEMPLATE = $('div />');
+MemoryGameView.TEMPLATE = $('<div />');
 
-MemoryGameView.ITEM_TEMPLATE = $('div />');
+MemoryGameView.ITEM_TEMPLATE = $('<div />').addClass('item');
