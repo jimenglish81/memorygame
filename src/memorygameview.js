2ef6383b4439923112bbdc615619566a7e0b91c8
jimenglish81 <jimenglish81@gmail.com>
2014-05-01 22:21:39 +0100

    view changes

--- src/memorygameview.js
+++ src/memorygameview.js
@@ -1,20 +1,30 @@
 function MemoryGameView(game, cssSelector) {
    this._game = game;
-   this._jContainer = $(cssSelector);
+   this._jContainer = $(cssSelector).css('position', 'relative');
 }
 
 MemoryGameView.prototype.render = function() {
-   var jDom = MemoryGameView.TEMPLATE.clone();
-   jDom.append(this._game.grid.map(MemoryGameView.prototype._buildItem, this))
-		.appendTo(this._jContainer);
-		
+    var jDom = MemoryGameView.TEMPLATE.clone().css({
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+    });
+   jDom.append(this._game.grid.map(MemoryGameView.buildItem.bind(null, this._game)))
+      .appendTo(this._jContainer);
+      
    return this;
 };
 
-MemoryGameView.prototype._buildItem = function(item, index) {
+MemoryGameView.buildItem = function(game, item) {
    var jItem = MemoryGameView.ITEM_TEMPLATE.clone()
-							.text(item.value)
-							.toggleClass('break', !(index % this._game.width));
+                     .text(item.value)
+                     .css({
+                        width: 100 / game.width + '%',
+                        height: 100 / game.height + '%'
+                      });
+   
    return jItem;
 };
 
