caabcca25cd8560cb23e1ad95df79ab1eec4a1b9
jimenglish81 <jimenglish81@gmail.com>
2014-05-02 11:46:50 +0100

    select item altered

--- src/memorygame.js
+++ src/memorygame.js
@@ -13,9 +13,9 @@ MemoryGame.prototype._initialise = function() {
 
    // as used by @steveukx
    this.grid = (new String(new Array(this.width * this.height)).split(',').map(function(value, index) { 
-     return new MemoryGame.Item(index % numberOfPairs + 1);
+      return new MemoryGame.Item(index % numberOfPairs + 1);
    }, this)).sort(function() { 
-     return 0.5 - Math.random(); 
+      return 0.5 - Math.random(); 
    });
    
    return this;
@@ -29,31 +29,37 @@ MemoryGame.prototype.selectItem = function(item) {
    var selectedLen = this._selected.length;
    
    if (item) {
-     if (selectedLen === 2) {
-       while(selectedLen--) {
-         this._selected.pop().exposed = false;
-       }
-     }
-     
-     item.exposed = true;
-     this._selected.push(item);
-     
-     if (this._selected.length === 2) {
-       if (this._selected[0].value === this._selected[1].value) {
-         var gameOver = !this._grid.filter(function(item) {
-            return !item.exposed;
-         }).length;
-         this._selected = [];
-         if (gameOver) {
-            console.log('won!');
+      if (this.hasMaximumSelected()) {
+         while(selectedLen--) {
+            this._selected.pop().exposed = false;
          }
-       }
-     }
-     
-     this._moves++;
+      }
+    
+      item.exposed = true;
+      this._selected.push(item);
+    
+      if (this.hasMaximumSelected()) {
+         if (this._selected[0].value === this._selected[1].value) {
+            this._selected.forEach(function(item) {
+               item.correct = true;
+            });
+            var gameOver = !this._grid.filter(function(item) {
+               return !item.correct;
+            }).length;
+            if (gameOver) {
+               console.log('won!');
+            }
+         }
+      }
+    
+      this._moves++;
    }
 
    return this;
+};
+
+MemoryGame.prototype.hasMaximumSelected = function() {
+   return this._selected.length === 2;
 };
 
 MemoryGame.WIDTH = 5;
@@ -63,4 +69,5 @@ MemoryGame.HEIGHT = 4;
 MemoryGame.Item = function(value) {
    this.value = value;
    this.exposed = false;
+   this.correct = false;
 };
